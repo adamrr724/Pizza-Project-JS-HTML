a11bfd8ee71804f445202323a7ab3359f4184b51
Adam Ross Russell <adamrr724@yahoo.com>
2016-01-29 14:20:24 -0800

    add styling and tweaks on UI

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ for (var i = 0; i < this.toppings.length; i++) {
   this.sizePizzaCost += 1;
 }
 return this.sizePizzaCost;
-}
+};
 
 AllPizzas.prototype.totalPrice = function () {
   var total = 0
@@ -41,7 +41,8 @@ $(document).ready(function() {
 
   $("#add-extra-pizza").click(function() {
     $("#new-pizzas").append('<div class="new-pizza"><h4>Please fill in the details for the next pizza:</h4><div class="size form-group"><label for="pizza-size">Choose the Size:</label> <br><input class="size-box" type="checkbox" value="Small"> Small <input class="size-box" type="checkbox" value="Medium"> Medium <input class="size-box" type="checkbox" value="Large"> Large </div><div class="toppings form-group"><label for="toppings">Choose your Toppings:</label> <br><h6>Veggies:</h6><input class="toppings-box" type="checkbox" value="Broccoli"> Broccoli <input class="toppings-box" type="checkbox" value="Peppers"> Peppers <input class="toppings-box" type="checkbox" value="Onion"> Onion <input class="toppings-box" type="checkbox" value="Garlic"> Garlic <input class="toppings-box" type="checkbox" value="Tomatoes"> Tomatoes <input class="toppings-box" type="checkbox" value="Olives"> Olives <input class="toppings-box" type="checkbox" value="Basil"> Basil <input class="toppings-box" type="checkbox" value="Arugala"> Arugala <input class="toppings-box" type="checkbox" value="Spinach"> Spinach <input class="toppings-box" type="checkbox" value="Mushrooms"> Mushrooms <br><h6>Meats:</h6><input class="toppings-box" type="checkbox" value="Pepperoni"> Pepperoni <input class="toppings-box" type="checkbox" value="Ham"> Ham <input class="toppings-box" type="checkbox" value="Meatball"> Meatball <input class="toppings-box" type="checkbox" value="Chicken"> Chicken <input class="toppings-box" type="checkbox" value="Sausage"> Sausage <input class="toppings-box" type="checkbox" value="Shrimp"> Shrimp <br><h6>Extras:</h6><input class="toppings-box" type="checkbox" value="Pineapple"> Pineapple <input class="toppings-box" type="checkbox" value="BBQ Sauce"> BBQ Sauce <input class="toppings-box" type="checkbox" value="Ranch"> Ranch <input class="toppings-box" type="checkbox" value="Extra Mozzarella"> Extra Mozzarella <input class="toppings-box" type="checkbox" value="Blue Cheese"> Blue Cheese <input class="toppings-box" type="checkbox" value="Parmasean">   Parmasean <input class="toppings-box" type="checkbox" value="Basil Pesto"> Basil Pesto </div></div>');
-                  });
+
+  });
 
 $("form#pizza-order").submit(function(event) {
   event.preventDefault();
@@ -50,14 +51,17 @@ $("form#pizza-order").submit(function(event) {
 
   $(".new-pizza").each(function() {
     var size = $(this).find(".size-box:checked").val();
-    debugger;
     var toppings = $(this).find(".toppings-box:checked").map(function() {
     return this.value;
     })
 
+    var toppingsView = $.makeArray(toppings);
+
+    $("#total").append("You have ordered 1 " + size.toLowerCase() + " pizza, with the following toppings: " + toppingsView.join(", ") + ".<br>");
     var newPizza = new Pizza(size, toppings);
 
     newAllPizzas.allSelections.push(newPizza.toppingsPricingMethod());
+
     });
 
     $("#total").append("The total price of this transaction comes to: $" + newAllPizzas.totalPrice());
